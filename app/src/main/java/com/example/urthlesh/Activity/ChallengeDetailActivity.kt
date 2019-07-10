05f6b32b1d5476eed5ab3ad0e765015a8e731121
gml3387 <gml33871010@gmail.com>
2019-07-10 23:15:20 +0900

    add some layout, activity

--- app/src/main/java/com/example/urthlesh/Activity/ChallengeDetailActivity.kt
+++ app/src/main/java/com/example/urthlesh/Activity/ChallengeDetailActivity.kt
@@ -3,11 +3,21 @@ package com.example.urthlesh.Activity
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import com.example.urthlesh.R
+import kotlinx.android.synthetic.main.activity_challenge_detail.*
 
 class ChallengeDetailActivity : AppCompatActivity() {
+    lateinit var title: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_challenge_detail)
+
+        var title = intent.getStringExtra("title")
+        txt_challenge_detail_title.text = title
+
+        btn_back_challenge_detail.setOnClickListener{
+            finish()
+        }
     }
+
 }
---
+++ app/src/main/java/com/example/urthlesh/ChallengeCreate/ActivityCreateCheck.kt
@@ -0,0 +1,13 @@
+package com.example.urthlesh.ChallengeCreate
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.urthlesh.R
+
+class ActivityCreateCheck : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_check)
+    }
+}
